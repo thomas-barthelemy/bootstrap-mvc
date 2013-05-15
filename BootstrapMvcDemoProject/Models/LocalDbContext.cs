2967a0c1659615f80b035b60fdcfe50ba0d1ef71
Draena_cp <03603ae22ec0b9765ee3c593890d1e96a2759d35E8XDybKn>
2013-05-15 05:38:08 +0000

    Added first version of demo project with only a simple menu so far.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS31]$/mvcextended;C23901

---
+++ BootstrapMvcDemoProject/Models/LocalDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BootstrapMvcDemoProject.Models
+{
+    public static class LocalDbContext
+    {
+        private static List<Student> _students;
+
+        public static ICollection<Student> Students
+        {
+            get { return _students ?? (_students = new List<Student>()); }
+        }
+    }
+}
---
+++ BootstrapMvcDemoProject/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BootstrapMvcDemoProject.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+    }
+}
